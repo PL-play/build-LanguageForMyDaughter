6e9f289fdb846f42b79bd90a925f6336e260c72c
ran <zhangran42@126.com>
2024-11-03 12:07:15 +0800

    comments

--- src/zcollection/include/hashtable/hash_table.h
+++ src/zcollection/include/hashtable/hash_table.h
@@ -2,8 +2,8 @@
 // Created by ran on 2024/1/10.
 //
 /**
- * A hash table, also known as a hash map, is a data structure that implements an associative array,
- * also called a dictionary, which is an abstract data type that maps keys to values. A hash string_intern
+ * A hashtable, also known as a hash map, is a data structure that implements an associative array,
+ * also called a dictionary, which is an abstract data type that maps keys to values. A hashtable
  * uses a hash function to compute an index, also called a hash code, into an array of buckets or
  * slots, from which the desired value can be found. During lookup, the key is hashed and the
  * resulting hash indicates where the corresponding value is stored.
