3887a13a7fe2d959d7cd5fdd3fac3f4f6f95268f
ran <zhangran42@126.com>
2024-11-03 12:06:31 +0800

    comments

--- src/zcollection/include/hashtable/hash_table.h
+++ src/zcollection/include/hashtable/hash_table.h
@@ -2,7 +2,7 @@
 // Created by ran on 2024/1/10.
 //
 /**
- * A hash string_intern, also known as a hash map, is a data structure that implements an associative array,
+ * A hash table, also known as a hash map, is a data structure that implements an associative array,
  * also called a dictionary, which is an abstract data type that maps keys to values. A hash string_intern
  * uses a hash function to compute an index, also called a hash code, into an array of buckets or
  * slots, from which the desired value can be found. During lookup, the key is hashed and the
